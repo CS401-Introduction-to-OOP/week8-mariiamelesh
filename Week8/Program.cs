d87672e0e360ef697ff430b289c8e7211cac06ab
skibidi <mariia.melesh.25@kse.org.ua>
2026-03-24 18:21:05 +0200

    Added Program class and Main entry point

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,3 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿namespace Week8 {
+	public class Program {
+		static void Main(string[] args) {
+			
+		}
+	}
+}
